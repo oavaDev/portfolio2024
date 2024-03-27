edc44b7464b1a659b2a5003ac8137471da5945f5
oava <omarvega598@gmail.com>
2024-03-26 20:11:46 -0500

    Fix(Experience): Removed duplicated experience

--- assets/experiences.tsx
+++ assets/experiences.tsx
@@ -11,19 +11,6 @@ const experiences = [
             "Created views, procedures and functions in the database in order to properly fit the requirements set on the project presentation. (Oracle / Postgres)",
             "I was able to learn a lot about the software development process and the technologies used in the project."
         ]
-    },
-    {
-        title: "It Analyst",
-        company: "Gralco SA",
-        date: "Apr 2023 - Present",
-        description: [
-            "Creation and implementation of effective technology solutions. Assessing business and end-user needs, solving technical challenges and assisting in dynamic IT environments. Working together with versatile teams to ensure successful completion of projects.",
-            "Developed  web applications for the company's internal use.",
-            "Worked on the frontend using React and Angular",
-            "Worked on the backend using Spring-boot.",
-            "Created views, procedures and functions in the database in order to properly fit the requirements set on the project presentation. (Oracle / Postgres)",
-            "I was able to learn a lot about the software development process and the technologies used in the project."
-        ]
     }
 ]
 export default  experiences ;
